8a36ddfd1546cb115c7890597da8ee28bd7f64ae
SabrinaRDC <SabrinaRDC.Dev+github@gmail.com>
2024-03-19 15:08:48 +0800

    added ability to updated more fields in db

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const dbConfig = {
     port: 3306,
     user: 'sbox-keygen',
     password: '',
-    database: 'sbox-keygen'
+    database: 'sbox-keygen',
+    multipleStatements: true
 };
 const serverConfig = {
     ip: '10.175.175.1',
@@ -37,7 +38,9 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/check', jsonParser, async (req, res) => {
-    console.log(req.method,req.url, req.body);
+    // const ip = req.headers['x-forwarded-for'].slice(req.headers['x-forwarded-for'].length/2+1);
+    const ip = req.socket.remoteAddress;
+    console.log(ip, req.method,req.url, req.body);
     let response = {};
     if (req.body.password !== checkPassAPI) {
         res.status(401);
@@ -65,7 +68,9 @@ app.post('/check', jsonParser, async (req, res) => {
 })
 
 app.post('/post', jsonParser, async (req, res) => {
-    console.log(req.method,req.url, req.body);
+    // const ip = req.headers['x-forwarded-for'].slice(req.headers['x-forwarded-for'].length/2+1);
+    const ip = req.socket.remoteAddress;
+    console.log(ip, req.method,req.url, req.body);
     let response = {};
     if (req.body.password !== postPassAPI ) {
         res.status(401);
@@ -80,10 +85,34 @@ app.post('/post', jsonParser, async (req, res) => {
         console.log(`New record created for ${req.body.ip}`);
     };
     res.status(200);
-    await query('UPDATE `sbox-keygen`.ips SET bigoted = ? WHERE ip = ?', [req.body.bigoted, req.body.ip]);
+    let SQLquery = '';
+    let SQLqueryData= [];
+    response = { ip: req.body.ip, message: "", bigoted_updated: false, banned_updated: false, ban_reason_updated: false};
+    if (req.body.bigoted != null) {
+        SQLquery = SQLquery.concat('UPDATE `sbox-keygen`.ips SET bigoted = ? WHERE ip = ?;');
+        SQLqueryData.push(req.body.bigoted, req.body.ip);
+        response.bigoted_updated = true;
+    };
+    if (req.body.banned != null) {
+        SQLquery = SQLquery.concat('UPDATE `sbox-keygen`.ips SET banned = ? WHERE ip = ?;');
+        SQLqueryData.push(req.body.banned, req.body.ip);
+        response.banned_updated = true;
+    };
+    if (req.body.ban_reason != null) {
+        SQLquery = SQLquery.concat('UPDATE `sbox-keygen`.ips SET ban_reason = ? WHERE ip = ?;');
+        SQLqueryData.push(req.body.ban_reason, req.body.ip);
+        response.ban_reason_updated = true;
+    };
+    if (!Boolean(SQLquery)) {
+        response.message = "No field specified to update!";
+        console.log('Responded with: ', response)
+        return res.send(response);
+    };
+    await query(SQLquery, SQLqueryData);
+    response.message = "Update successful!";
     console.log(`Updated record for ${req.body.ip}`);
-    response = { message: "Success!" };
     console.log('Responded with: ', response);
-    res.send(response);
+    return res.send(response);
+    
 })
 
